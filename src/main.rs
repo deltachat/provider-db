f0f65de547a8425b726f9695c1f5722572c8b20c
Simon Laux <mobile.info@simonlaux.de>
2019-07-22 23:21:07 +0200

    get provider by domain

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,20 @@
 #[allow(dead_code)]
 #[derive(Debug)]
-enum StatusState {
+pub enum StatusState {
     OK,
     PREPARATION,
     BROKEN,
 }
 #[allow(dead_code)]
 #[derive(Debug)]
-struct Status {
+pub struct Status {
     state: StatusState,
     date: &'static str,
 }
 
 #[allow(dead_code)]
 #[derive(Debug)]
-struct Provider {
+pub struct Provider {
     overview_page: &'static str, // for providers.delta.chat/{overview_page}
     name: &'static str,
     status: Status,
@@ -31,12 +31,32 @@ struct DomainDBEntry {
 include!(concat!(env!("OUT_DIR"), "/data.rs"));
 
 fn main() {
-    println!("{:?}", DATABASE[29]);
+    println!("{:?}", get_provider_info("mailbox.org"));
+
+}
+
+
+pub fn get_provider_info( domain:&str ) -> Option<(&Provider, Vec<&'static str>)> {
+    let domain_search_res:Option<&DomainDBEntry> = DOMAIN_DB.iter().find(|e| e.domain == domain);
+    if domain_search_res.is_some() {
+    let provider_id:u32 = domain_search_res.unwrap().list_index;
+      return Some(
+        (&DATABASE[provider_id as usize], get_domains_by_provider(provider_id))
+      )  
+    } else {
+        return None
+    }
+       
+}
+
+fn get_domains_by_provider(provider_id:u32) -> Vec<&'static str> {
+    return DOMAIN_DB.iter()
+        .filter(|entry| entry.list_index == provider_id)
+        .map(|e| e.domain).collect();
 }
 
 /*
-[] (exported) get domain name from valid email address
-[] (exported) get provider and its domains from domain name
-[] ? (exported) search by name
-[] find all domains with provider id
+[ ] (exported) get domain name from valid email address
+[X] (exported) get provider and its domains from domain name
+[X] find all domains with provider id
 */
