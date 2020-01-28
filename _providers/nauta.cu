c88b98882e313e348da868b41f5b680d66ae57ce
missytake <missytake@systemli.org>
2020-01-28 10:47:51 +0100

    added nauta.cu #57

---
+++ _providers/nauta.cu
@@ -0,0 +1,21 @@
+---
+name: nauta.cu
+domains:
+  - nauta.cu
+status: OK
+server:
+  - type: imap
+    socket: NONE
+    hostname: imap.nauta.cu
+    port: 143
+  - type: smtp
+    socket: NONE
+    hostname: smtp.nauta.cu
+    port: 25
+after_login_hint: |
+    Attención - con nauta.cu, puedes enviar mensajes sólo a un máximo de 20 personas. 
+    En grupos más grandes que eso, no puedes dejar o enviar mensajes.
+last_checked: 2020-01
+website: https://nauta.cu
+---
+
